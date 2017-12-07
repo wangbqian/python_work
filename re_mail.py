8a1895a27355fd3269df9fac6f74a7069c4b6cb8
wengfuyi <214952026@qq.com>
2017-12-07 10:25:47 +0800

    邮箱姓名提取

--- re_mail.py
+++ re_mail.py
@@ -2,13 +2,34 @@ import re
 
 def is_valid_email(addr):
     if re.match(r'^([0-9a-zA-Z\w.]*)(@)(\w*)\.(\w{3})$',addr):
+        print('%s is a email' % addr)
         return True
     else:
+        print('%s is not a email' %addr)
         return False
-        
+
 
 assert is_valid_email('someone@gmail.com')
 assert is_valid_email('bill.gates@microsoft.com')
 assert not is_valid_email('bob#example.com')
 assert not is_valid_email('mr-bob@example.com')
+print('ok')
+
+
+def name_of_email(addre):
+    m = re.match(r'^(<(?P<name>.+?)>)?\s*(?P<name1>[a-zA-Z]+)@([a-zA-Z]+)[.]([a-zA-Z]{3})',addre)
+    firstName = m.group('name')
+    secondName = m.group('name1') 
+    if m :
+        if firstName is not None:
+            print('%s => %s' % (addre,firstName))
+            return firstName
+        else:
+            print('%s => %s' % (addre,secondName))
+            return secondName
+    else:
+        print('%s is not a email' %addr)
+
+assert name_of_email('tom@voyager.org') == 'tom'
+assert name_of_email('<Tom Paris> tom@voyager.org') == 'Tom Paris'
 print('ok')
