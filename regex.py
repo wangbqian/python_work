24a65e9d906d1c6b92531f47279f008409e4daea
wengfuyi <214952026@qq.com>
2017-12-13 21:59:21 +0800

    contextlib

--- regex.py
+++ regex.py
@@ -4,15 +4,15 @@ someotherthing"""
 
 import re
 
-print re.match('some', string_with_newlines) # matches
-print re.match('someother', string_with_newlines) # won't match 
-print re.match('^someother', string_with_newlines, re.MULTILINE) # also won't match
-print re.search('someother', string_with_newlines) # finds something
-print re.search('^someother', string_with_newlines, re.MULTILINE) # also finds something
+print(re.match('some', string_with_newlines)) # matches
+print (re.match('someother', string_with_newlines)) # won't match 
+print (re.match('^someother', string_with_newlines, re.MULTILINE)) # also won't match
+print (re.search('someother', string_with_newlines)) # finds something
+print (re.search('^someother', string_with_newlines, re.MULTILINE)) # also finds something
 
 m = re.compile('thing$', re.MULTILINE)
 
-print m.match(string_with_newlines) # no match
-print m.match(string_with_newlines, pos=4) # matches
-print m.search(string_with_newlines) # also matches
-print m.search(string_with_newlines,re.MULTILINE) # also matches
+print (m.match(string_with_newlines)) # no match
+print (m.match(string_with_newlines, pos=4)) # matches
+print (m.search(string_with_newlines)) # also matches
+print(m.search(string_with_newlines,re.MULTILINE)) # also matches
