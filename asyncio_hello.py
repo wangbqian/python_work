8b69d12da5efa597bf09eb9ebc0b159c7444739f
wengfuyi <214952026@qq.com>
2017-12-25 15:51:20 +0800

    asyncio 使用async/await 代替 yield from 旧语法

--- asyncio_hello.py
+++ asyncio_hello.py
@@ -20,24 +20,44 @@ import threading
 
 #--------- Hello world----------------#
 
-@asyncio.coroutine
-def wget(host):
-    print('wget %s ...' % host)
+# @asyncio.coroutine
+# def wget(host):
+#     print('wget %s ...' % host)
+#     connect = asyncio.open_connection(host,80)
+#     reader,writer = yield from connect
+#     header = 'GET / HTTP/1,0\r\nHost:%s\r\n\r\n' %host
+#     writer.write(header.encode('utf-8'))
+#     yield from writer.drain()
+#     while True:
+#         line = yield from reader.readline()
+#         if line == b'\r\n':
+#             break
+#         print('%s header > %s' %(host,line.decode('utf-8').rstrip()))
+#         writer.close()
+
+
+# loop = asyncio.get_event_loop()
+# tasks = [wget(host) for host in ['www.sina.com.cn','www.sohu.com','www.163.com']]
+# loop.run_until_complete(asyncio.wait(tasks))
+# loop.close()
+#--------------异步网络连接来获取sina、sohu和163的网站首页-------------#
+
+async def wget(host):
+    print('wget %s ...' %host)
     connect = asyncio.open_connection(host,80)
-    reader,writer = yield from connect
-    header = 'GET / HTTP/1,0\r\nHost:%s\r\n\r\n' %host
+    reader,writer = await connect
+    header = 'GET / HTTP/1.0\r\nHost:%s\r\n\r\n' % host
     writer.write(header.encode('utf-8'))
-    yield from writer.drain()
+    await writer.drain()
     while True:
-        line = yield from reader.readline()
+        line = await reader.readline()
         if line == b'\r\n':
             break
         print('%s header > %s' %(host,line.decode('utf-8').rstrip()))
         writer.close()
 
-
 loop = asyncio.get_event_loop()
 tasks = [wget(host) for host in ['www.sina.com.cn','www.sohu.com','www.163.com']]
 loop.run_until_complete(asyncio.wait(tasks))
 loop.close()
-#--------------异步网络连接来获取sina、sohu和163的网站首页-------------#
+#------------- 使用 async/await 代替 yield from 旧语法----------------#
