1715dba672c5613ef5b4f18daab385246a7a5533
wengfuyi <214952026@qq.com>
2018-02-05 16:18:36 +0800

    SQLAlchemy  ORM 和 core API 调用 方言的使用。 pycharm 工程

--- SQLAlchemy.py
+++ SQLAlchemy.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column,String,create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column,String,create_engine,ForeignKey
+from sqlalchemy.orm import sessionmaker,relationship
 from sqlalchemy.ext.declarative import declarative_base
+import json
 
 
 """
@@ -16,6 +17,44 @@ class User(Base):
 
     id = Column(String(20),primary_key=True)
     name = Column(String(20))
+    # 一对多:
+    books = relationship('Book')
+
+    def __str__(self):
+        return 'user: id = %s,name = %s,books = %s' % (self.id, self.name,self.book)
+
+    __repr__ = __str__
+
+class Book(Base):
+    __tablename__ = 'book'
+
+    id = Column(String(20),primary_key=True)
+    name = Column(String(20))
+    # “多”的一方的book表是通过外键关联到user表的:
+    user_id = Column(String(20),ForeignKey('user.id'))
+
+    #定义实例打印样式
+    # def __str__(self):
+    #     return 'book: id = %s,name = %s,user_id = %s' % (self.id, self.name, self.user_id)
+    # __repr__ = __str__
+    def __repr__(self):
+       return 'book: id = %s,name = %s,user_id = %s' % (self.id, self.name, self.user_id) 
+    
+
+def tojson(self):
+    if isinstance(self, User):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'book': self.book
+        }
+    else:
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id
+        }
+
 # 初始化数据库连接:
 engine = create_engine('mysql+mysqlconnector://root:admin@localhost:3306/test')
 
@@ -24,23 +63,43 @@ DBSession = sessionmaker(bind=engine)
 
 #向数据库中添加记录
 # 创建session对象:
-session = DBSession()
-# 创建新User对象:
-new_user = User(id='5', name='Bob')
-# 添加到session:
-session.add(new_user)
-# 提交即保存到数据库:
-session.commit()
-# 关闭session:
-session.close()
+# session = DBSession()
+# # 创建新User对象:
+# new_user = User(id='5', name='Bob')
+# # 添加到session:
+# session.add(new_user)
+# # 提交即保存到数据库:
+# session.commit()
+# # 关闭session:
+# session.close()
 
 
 # #从数据库中查询数据
-# session = DBSession()
+session = DBSession()
 
 # user = session.query(User).filter(User.id == '5').one()
+users = session.query(User).all()
+print('user type :', type(users))
+# print('id :',user.id)
+# print('name :',user.name)
+for u in users:
+    # print('book type :',type(u.book))
+    print(u)
+print(users)
+print('---------------------------'*3)
 
-# print('type: ',type(user))
-# print('name: ',user.name)
+u = session.query(User).filter(User.id == '5').one()
+# print(u)
+print(json.dumps(users, default=tojson,indent=4))
 
 session.close()
+
+
+# session = DBSession()
+
+# user = session.query(Book).filter(Book.id == '1').one()
+
+# print('type: ',type(book))
+# print('name: ',book.name)
+
+# session.close()
